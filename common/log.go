9ced86608a5a3ffed93c2a4b3f34ac7943226a9e
nerowander <2497091708@qq.com>
2025-04-17 16:54:35 +0800

    Initial commit: init 1.3

--- common/log.go
+++ common/log.go
@@ -88,8 +88,14 @@ func logSave() {
 func WriteLogToFile(result string, filename string) {
 	// json or html
 	// 最好加一个时间戳的文件
-
-	file, err := os.OpenFile(filepath.Join("./output", filename), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	var err error
+	err = os.MkdirAll("./output", os.ModePerm)
+	if err != nil {
+		fmt.Println("Error creating output directory:", err)
+		return
+	}
+	var file *os.File
+	file, err = os.OpenFile(filepath.Join("./output", filename), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		fmt.Printf("Open file %s error, %v\n", filename, err)
 		return
